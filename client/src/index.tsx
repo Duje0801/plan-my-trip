d05b478e147e4341dd6df6793fc4a1268a3ba10e
duje <dzemunik@yahoo.com>
2024-01-31 12:51:58 +0100

    uncommented strict mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  //<React.StrictMode>
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </React.StrictMode>
 );
