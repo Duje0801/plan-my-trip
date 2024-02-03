d15ee8368667120a865308f48568f68f789f083e
duje <dzemunik@yahoo.com>
2024-02-03 20:33:12 +0100

    added helmet and rate limiter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import cors from "cors";
 
 //Routes imports
@@ -7,16 +9,29 @@ import search from "./controller/search";
 import trip from "./controller/trip";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: `10kb` }));
 
 dotenv.config();
+
+//Additional security (headers)
+app.use(helmet());
+
+//Rate limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 3600000,
+  message: `Too many requests from this IP address, please try again in 60 minutes.`,
+});
+app.use(limiter);
+
+//Allows access from other domains (front-end)
+app.use(cors());
 
 //Routes
 app.get(`/api/search/:id`, search);
 app.get(`/api/trip/`, trip);
 
-//Server
+//Connecting to server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
